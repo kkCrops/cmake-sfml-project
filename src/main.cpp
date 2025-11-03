d8a0c1d8558f6f1b4c292fe61b4ebe957dc01ba9
kkCrops <kliang2006@gmail.com>
2025-11-03 15:39:02 -0500

    Milestone 1 - Shapes

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,40 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    // create the window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
 
+    // run the program as long as the window is open
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         while (const std::optional event = window.pollEvent())
         {
+            // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
 
-        window.clear();
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        sf::CircleShape shape(100.f);
+        shape.setPosition({400-100,300-100});
+        shape.setFillColor(sf::Color(100, 250, 50));
+
+        shape.setOutlineThickness(5.f);
+        shape.setOutlineColor(sf::Color(255, 255, 255));
+
+        window.draw(shape);
+
+        sf::CircleShape square(100, 4);
+        square.setPosition({400-100,300-100});
+        square.setFillColor(sf::Color(100, 50, 50));
+
+        window.draw(square);
+
+        // end the current frame
         window.display();
     }
 }
